bdf99e4939d6e6f0f29a763ac685e965548a0401
Filipe Mendonça <filipesm.7@gmail.com>
2019-02-21 23:05:08 +0000

    Added Api classes for request and response handling

---
+++ producer/class/Api.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'ApiResponse.php';
+require_once 'DBHandler.php';
+require_once dirname( __FILE__ ) . '/../models/UserModel.php';
+require_once dirname( __FILE__ ) . '/../controllers/UserController.php';
+
+class Api {
+	
+	private $response;
+	
+	public function __construct() {}
+	
+	public function get_response() {
+		return $this->response;
+	}
+	
+	public function set_response( ApiResponse $response ){
+		$this->response = $response;
+	}
+	
+	public function execute( $action, $params ){
+		$this->response = new ApiResponse( "401", "unknown action" );
+		
+		switch ( $action ) {
+			case "logon": {
+				$dbh = DBHandler::get_instance();
+				$user = new UserController (
+					new UserModel( $dbh )
+				);
+				
+				$result = $user->login( $params["username"] );
+
+				$successful = !empty( $result );
+				$this->response->set_status( $successful ? "200" : "401" );
+				$this->response->set_message( $successful ? "login successful" : "user does not exist"  );
+			}
+		}
+	}
+}
