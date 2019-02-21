88736080ed22f4608c105feda78db72140a503c3
Filipe Mendonça <filipesm.7@gmail.com>
2019-02-21 23:08:10 +0000

    Added configuration file to producer

---
+++ producer/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/* PRODUCER CONFIGURATION */
+
+//DB CONFIGURATION
+global $helloprint_config;
+
+$helloprint_config["DNS"] = "mysql:dbname=helloprint;host=127.0.0.1";
+$helloprint_config["DBUSER"] = "hprint";
+$helloprint_config["DBPASSWORD"] = "12345";
