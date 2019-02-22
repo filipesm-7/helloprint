bf7d4dc1cea67d52f0d2eb485d959c4c749228d8
Filipe Mendonça <filipesm.7@gmail.com>
2019-02-22 19:13:13 +0000

    Added login api request and handling, refactor user message display

--- webserver/assets/js/login.js
+++ webserver/assets/js/login.js
@@ -1,4 +1,30 @@
-function login() {
+function login( user, pass ) {
+    var result = {}
+    var method = "POST";
+    var endpoint = helloprint.PRODUCER_SERVER + helloprint.PRODUCER_LOGIN_ENDPOINT.replace( "{username}", user );
+    
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(e) {
+        if ( xhr.readyState === 4 ) {
+            var message = "";
+            var opts = { classname: "text-danger" };
+             
+            if ( xhr.status === 200 ) {
+                var response = JSON.parse( xhr.response );
+                
+                message = response.message;
+                opts.classname = "text-info";
+            }
+            else {
+                message = "problem communicating with server"
+            }
+            
+            helloprint.utils.show_form_message( "form-message", message, opts );
+        }
+    }
+    xhr.open( method, endpoint, true );
+    xhr.setRequestHeader( "Content-type", "application/x-www-form-urlencoded" );
+    xhr.send( "password=" + pass );
 }
 
 function request_password() {
@@ -8,6 +34,7 @@ function validate_login() {
     var fusername = document.getElementById( "username" ).value;
     var fpassword = document.getElementById( "password" ).value;
     var invalid_fields = [];
+    var message = "";
 
     if ( fusername == "" ) {
         invalid_fields.push( {"field": "username", "message": "username is required"} );
@@ -18,16 +45,12 @@ function validate_login() {
     }
     
     if( invalid_fields.length === 0 ){  //no errors, proceed to login request
-        login();
+        login( fusername, fpassword );
+    } else {
+        //add error classes to fields and update user message
+        helloprint.utils.show_form_errors( invalid_fields );
+        helloprint.utils.show_form_message( "form-message", invalid_fields[0].message );
     }
-    
-    //add error classes to fields
-    helloprint.utils.show_form_errors( invalid_fields );
-    
-    //show first field error message
-    document.getElementById( "form-message" ).style.display = "block";
-    document.getElementById( "form-message" ).className = "text-danger";
-    document.getElementById( "form-message" ).textContent = invalid_fields[0].message;
 
     return false;
 }
