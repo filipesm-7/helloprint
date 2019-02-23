85ecf97a55c9d2847c936884ac78fff73093aa96
Filipe Mendonça <filipesm.7@gmail.com>
2019-02-23 11:48:35 +0000

    Added RabbitMQ queue configuration and queue names

--- producer/src/Configuration.php
+++ producer/src/Configuration.php
@@ -7,9 +7,19 @@ class Configuration {
     const REQUEST_LOGON = "logon";
     const REQUEST_REQUESTPASSWORD = "request_password";
     
+    const QUEUE_RPASSWORD = "rpassword";
+    const QUEUE_EMAILSENDER = "email";
+    
     static $DB = array (
         "DNS"           => "mysql:dbname=helloprint;host=127.0.0.1",
         "USER"          => "hprint",
         "PASSWORD"      => "12345"
+    );
+    
+    static $QUEUE = array (
+        "SERVER"        => "localhost",
+        "PORT"          => "5672",
+        "USER"          => "guest",
+        "PASS"          => "guest"
     );
 }
