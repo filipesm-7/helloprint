952a78c34bda7f4dabe41f5ef86356444710a8e4
Filipe Mendonça <filipesm.7@gmail.com>
2019-02-23 19:46:50 +0000

    Removed unneeded constant, added comments

--- producer/src/Configuration.php
+++ producer/src/Configuration.php
@@ -4,18 +4,21 @@ namespace Helloprint;
 
 class Configuration {
     
+    //api request actions
     const REQUEST_LOGON = "logon";
     const REQUEST_REQUESTPASSWORD = "request_password";
     
+    //request password queue namespace
     const QUEUE_RPASSWORD = "rpassword";
-    const QUEUE_EMAILSENDER = "email";
     
+    //db configuration
     static $DB = array (
         "DNS"           => "mysql:dbname=helloprint;host=127.0.0.1",
         "USER"          => "hprint",
         "PASSWORD"      => "12345"
     );
     
+    //rabbitmq configuration
     static $QUEUE = array (
         "SERVER"        => "localhost",
         "PORT"          => "5672",
