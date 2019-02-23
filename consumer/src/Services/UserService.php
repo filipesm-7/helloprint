b3304711b0a809dc38a27f9aed6d4404d0918ea3
Filipe Mendonça <filipesm.7@gmail.com>
2019-02-23 22:53:20 +0000

    Remove unneeded methods

--- consumer/src/Services/UserService.php
+++ consumer/src/Services/UserService.php
@@ -11,32 +11,6 @@ class UserService {
 	public function __construct( $dbh ) {
 		$this->dbh = $dbh;
 	}
-	
-	public function get_dbh() {
-		return $this->dbh;
-	}
-	
-	public function get_user( $username, $filters=array() ){
-		$pdo = $this->dbh->init_connection();
-		
-		$sql = "SELECT * FROM users WHERE username = :username";
-		
-		if ( !empty( $filters ) ) {
-			foreach( $filters as $key => $value ) {
-				$sql .= " AND " . $key . " = :" . $key;
-			}
-		}
-		$filters[":username"] = $username;
-        
-		$sth = $pdo->prepare( $sql );
-		$sth->execute( $filters );
-		$result = $sth->fetch( PDO::FETCH_ASSOC );
-		
-		//close connection
-		$pdo = null;
-		
-		return $result;
-	}
     
     public function update_password( $userid, $password ) {
         $pdo = $this->dbh->init_connection();
