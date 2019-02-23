f8f63703f45ed29c4ac6f1d60bc4b6ec72706bfc
Filipe Mendonça <filipesm.7@gmail.com>
2019-02-23 11:50:31 +0000

    Added producer logic to request password

--- producer/src/Controllers/UserController.php
+++ producer/src/Controllers/UserController.php
@@ -2,9 +2,9 @@
 
 namespace Helloprint\Controllers;
 
+use HelloPrint\Configuration;
 use HelloPrint\Models\UserModel;
 
-use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 class UserController {
@@ -17,12 +17,32 @@ class UserController {
 	
 	public function login( $username, $password ){
 		$filters = array( 
-			":password" => $password,
-			":status" 	=> 1
+			"password" => $password,
+			"status" 	=> 1
 		);
 		return $this->model->get_user( $username, $filters );
 	}
     
     public function request_password( $username ) {
+        $user = $this->model->get_user( $username, array ( "status" => 1 ) );
+        
+        if ( empty( $user ) ) {
+            return false;
+        }
+        
+        try {
+            $queue = new \Helloprint\AMQPWrapper();
+            
+            $msg = new AMQPMessage( $user["username"] );
+            $queue->channel->basic_publish( 
+                $msg, 
+                "",
+                Configuration::REQUEST_REQUESTPASSWORD
+            );
+        } catch( \Exception $e ) {
+            return false;
+        }
+
+        return true;
     }
 }
