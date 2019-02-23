eca31a8fc6e9b90dfa09e2c0cefa15363fc17eb0
Filipe Mendonça <filipesm.7@gmail.com>
2019-02-23 10:54:33 +0000

    Added constants for request actions

--- producer/src/Configuration.php
+++ producer/src/Configuration.php
@@ -4,6 +4,9 @@ namespace Helloprint;
 
 class Configuration {
     
+    const REQUEST_LOGON = "logon";
+    const REQUEST_REQUESTPASSWORD = "request_password";
+    
     static $DB = array (
         "DNS"           => "mysql:dbname=helloprint;host=127.0.0.1",
         "USER"          => "hprint",
