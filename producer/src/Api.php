8d2dac2874584074452accc7e38e93ce02fc0c9c
Filipe Mendonça <filipesm.7@gmail.com>
2019-02-23 10:58:06 +0000

    Refactor api routing for decreased method responsability and repetition

--- producer/src/Api.php
+++ producer/src/Api.php
@@ -22,37 +22,48 @@ class Api {
     }
     
     public function execute( $action, $params ){
-        $this->response = new ApiResponse( "401", "unknown action" );
         
-        switch ( $action ) {
-            case "logon": {
-                $dbh = DBHandler::get_instance();
-                $user = new UserController (
-                    new UserModel( $dbh )
+        $controller = $this->route( $action );
+        if ( $controller == null ) {
+            $this->response = new ApiResponse( "401", "unknown action" );
+            return;
+        }
+        
+        $classname = (new \ReflectionClass($controller))->getShortName();
+        switch ( $classname ) {
+            case "UserController": {
+                $message = "";
+                if ( $action == Configuration::REQUEST_LOGON ) {
+                    $result = $controller->login( $params["username"], $params["password"] );
+                    $message = !empty( $result ) ? "login successful" : "user does not exist";
+                }
+                
+                elseif ( $action == Configuration::REQUEST_REQUESTPASSWORD ) {
+                    $result = $controller->request_password( $params["username"] );
+                    $message = $result ? "password request made - you will receive an email shortly" : "unable to request password, please try again";
+                }
+                
+                $this->response = new ApiResponse(
+                    ( !empty( $result ) ? "200" : "401" ),
+                    $message
                 );
-                
-                $result = $user->login( $params["username"], $params["password"] );
-
-                $successful = !empty( $result );
-                $this->response->set_status( $successful ? "200" : "401" );
-                $this->response->set_message( $successful ? "login successful" : "user does not exist"  );
-                
-                break;
-            }
-            case "request_password": {
-                $dbh = DBHandler::get_instance();
-                $user = new UserController (
-                    new UserModel( $dbh )
-                );
-                
-                $result = $user->request_password( $params["username"] );
-                
-                $successful = !empty( $result );
-                $this->response->set_status( $successful ? "200" : "401" );
-                $this->response->set_message( $successful ? "password request made - you will receive an email shortly" : "unable to request password, please try again"  );
-                
                 break;
             }
         }
+    }
+    
+    public function route( $action ){
+        $controller = null;
+        $dbh = DBHandler::get_instance();
+  
+        switch ( $action ) {
+            case Configuration::REQUEST_LOGON:
+            case Configuration::REQUEST_REQUESTPASSWORD: {
+                $controller = new UserController (
+                    new UserModel( $dbh )
+                );
+            }
+        }
+        return $controller;
     }
 }
