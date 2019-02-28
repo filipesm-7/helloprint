ddca88d28d57188208e308bae672d282d3f264b8
Filipe Mendonça <filipesm.7@gmail.com>
2019-02-28 21:28:04 +0000

    Changed service method to allow multiple fields to be updated

--- consumer/src/Services/UserService.php
+++ consumer/src/Services/UserService.php
@@ -12,12 +12,30 @@ class UserService {
 		$this->dbh = $dbh;
 	}
     
-    public function update_password( $userid, $password ) {
+    public function update_user( $userid, $fields ) {
+        $allowed_fields = array( "password", "status" );
+        $update_fields = array();
+
+        foreach( $allowed_fields as $field ) { 
+
+            if( empty( $fields[$field] ) ){     //remove field from update if it's not whitelisted
+                unset( $fields[$field] );
+                continue;
+            }
+            
+            $update_fields[] = $field . " = :" . $field;
+        }
+        
+        if( empty( $update_fields ) ){
+            return false;
+        }
+        $fields["userid"] = $userid;
+        
         $pdo = $this->dbh->init_connection();
 		
-		$sql = "UPDATE users SET password = :password WHERE Id = :userid";
-        
+		$sql = "UPDATE users SET " . implode( ",", $update_fields ) . " WHERE Id = :userid";
         $sth = $pdo->prepare( $sql );
-		return $sth->execute( array ( "userid" => $userid, "password" => $password ) );	
+        
+		return $sth->execute( $fields );
     }
 }
